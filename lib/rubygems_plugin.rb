00ce512d7b13b470f442bb6ada6d090753da4772
Akira Matsuda <ronnie@dio.jp>
2012-10-13 07:59:32 +0900

    tweak GitHub URL variations

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,9 +1,24 @@
 require 'rubygems'
 
 Gem.post_install do |installer|
-  if installer.spec.homepage && !installer.spec.homepage.empty? && !File.exists?("#{installer.gem_dir}/src") && !`git ls-remote #{installer.spec.homepage} 2> /dev/null`.empty?
-    Dir.chdir installer.gem_dir do
-      `git clone #{installer.spec.homepage} src`
+  if installer.spec.homepage && !installer.spec.homepage.empty? && !File.exists?("#{installer.gem_dir}/src")
+    repo = installer.spec.homepage
+    if repo =~ /\Ahttps?:\/\/([^.]+)\.github.com\/(.+)/
+      repo = if $1 == 'www'
+        "https://github.com/#{$2}"
+      elsif $1 == 'wiki'
+        # https://wiki.github.com/foo/bar => https://github.com/foo/bar
+        "https://github.com/#{$2}"
+      else
+        # https://foo.github.com/bar => https://github.com/foo/bar
+        "https://github.com/#{$1}/#{$2}"
+      end
+    end
+
+    if !`git ls-remote #{repo} 2> /dev/null`.empty?
+      Dir.chdir installer.gem_dir do
+        `git clone #{repo} src`
+      end
     end
   end
 end
