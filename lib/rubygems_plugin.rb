355b55e37533a34fa8950c27e03eb3f9ff96ef64
Akira Matsuda <ronnie@dio.jp>
2012-10-12 19:47:43 +0900

    '' is never a Git repo

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 
 Gem.post_install do |installer|
-  if installer.spec.homepage && !File.exists?("#{installer.gem_dir}/src") && !`git ls-remote #{installer.spec.homepage} 2> /dev/null`.empty?
+  if installer.spec.homepage && !installer.spec.homepage.empty? && !File.exists?("#{installer.gem_dir}/src") && !`git ls-remote #{installer.spec.homepage} 2> /dev/null`.empty?
     Dir.chdir installer.gem_dir do
       `git clone #{installer.spec.homepage} src`
     end
