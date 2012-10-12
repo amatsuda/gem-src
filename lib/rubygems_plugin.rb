94b3f07b80e70eb7dfedd48d201203c6c67e9dd3
Hiroshige Umino <hiroshige88@gmail.com>
2012-10-13 00:29:14 +0900

    Enable to set clone locations with env

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 
 Gem.post_install do |installer|
+  clone_root = ENV['GEMSRC_CLONE_ROOT'] || installer.gem_dir
+  clone_dir = ENV['GEMSRC_CLONE_DIR'] || (clone_root == installer.gem_dir ? 'src' : installer.spec.name)
   if installer.spec.homepage && !installer.spec.homepage.empty? && !File.exists?("#{installer.gem_dir}/src") && !`git ls-remote #{installer.spec.homepage} 2> /dev/null`.empty?
-    Dir.chdir installer.gem_dir do
-      `git clone #{installer.spec.homepage} src`
-    end
+    `git clone #{installer.spec.homepage} #{clone_root}/#{clone_dir}`
   end
 end
